e982e0fe484bf536e66c50c2a74f7e56fe846bfa
meidanlong <meidanlong@gmail.com>
2023-08-22 15:02:27 +0800

    恢复前端调用地址

--- ruoyi-ui-vue3/vite.config.js
+++ ruoyi-ui-vue3/vite.config.js
@@ -30,8 +30,8 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          // target: 'http://localhost:8080',
-          target: 'http://82.156.216.48:8080',
+          target: 'http://localhost:8080',
+          // target: 'http://82.156.216.48:8080',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
